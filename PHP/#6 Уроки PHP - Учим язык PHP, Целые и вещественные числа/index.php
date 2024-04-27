ecc0d42e5a7b43a3f771e9895e651ce6f18ebbff
Kirill <black.star.ibhbr@gmail.com>
2024-04-27 08:15:17 +0500

    урок 6

---
+++ PHP/#6 Уроки PHP - Учим язык PHP, Целые и вещественные числа/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?= "Страница"; ?></title>
+</head>
+<body>
+	<?php
+		echo 04+05;
+		echo "<br>";
+		echo 0x12d+0x64f;
+	?>
+</body>
+</html>
