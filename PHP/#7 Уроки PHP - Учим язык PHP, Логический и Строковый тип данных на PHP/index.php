663b68cab03118345b5fe69916af7242bbdf2816
Kirill <black.star.ibhbr@gmail.com>
2024-04-27 08:30:41 +0500

    Урок 7

---
+++ PHP/#7 Уроки PHP - Учим язык PHP, Логический и Строковый тип данных на PHP/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?= "Страница"; ?></title>
+</head>
+<body>
+	<?php
+		$str ="Строка";
+		echo "{$str}'lskdjgslg' <br>";
+		echo "\$str <br>";
+		echo `dir`;
+	?>
+</body>
+</html>
+
+<!--
+	кавычки
+	"" - обычные
+	'' - в этих можно писать в перемнной
+	`` - выполняют системную команда
+-->
