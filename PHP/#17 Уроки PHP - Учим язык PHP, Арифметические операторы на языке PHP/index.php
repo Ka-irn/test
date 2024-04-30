31c8f53de6b31076897933a3068ebc8084193185
Kirill <black.star.ibhbr@gmail.com>
2024-04-30 21:04:45 +0500

    урок 17

---
+++ PHP/#17 Уроки PHP - Учим язык PHP, Арифметические операторы на языке PHP/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?= "Страница"; ?></title>
+</head>
+<body>
+	<?php
+		if(21 % 4) echo 'не чётно';
+		else echo 'чётно';	
+	?>
+</body>
+</html>
