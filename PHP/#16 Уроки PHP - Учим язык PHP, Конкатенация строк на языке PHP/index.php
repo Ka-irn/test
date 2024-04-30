91bd96e4c3f60be1f27e6aca9403133c342d981c
Kirill <black.star.ibhbr@gmail.com>
2024-04-30 20:49:10 +0500

    Урок 16

---
+++ PHP/#16 Уроки PHP - Учим язык PHP, Конкатенация строк на языке PHP/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?= "Страница"; ?></title>
+</head>
+<body>
+	<?php
+		$dg = 3;
+		$dg .= ' aafaw';
+		echo $dg . ' =Конкатенация';
+	?>
+</body>
+</html>
