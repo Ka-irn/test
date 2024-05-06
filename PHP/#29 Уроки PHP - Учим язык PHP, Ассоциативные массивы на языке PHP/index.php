973144fe5bb218c0a16b0536fbfdee4a18a34ecc
Kirill <black.star.ibhbr@gmail.com>
2024-05-06 14:55:37 +0500

    Урко 29

---
+++ PHP/#29 Уроки PHP - Учим язык PHP, Ассоциативные массивы на языке PHP/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?= "Страница"; ?></title>
+</head>
+<body>
+	<?php
+		$ar = ['один' => "1", 'два' => "2", 'три' => "3"];
+		echo '<pre>';
+		print_r($ar);
+	?>
+</body>
+</html>
